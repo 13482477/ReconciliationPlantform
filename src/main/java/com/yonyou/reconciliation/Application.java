3dd3a0f0cb56174fe4151fab7ffd5ab876a9843f
lizhiqiang <13482477@qq.com>
2018-01-08 15:12:53 +0800

    commit

--- src/main/java/com/yonyou/reconciliation/Application.java
+++ src/main/java/com/yonyou/reconciliation/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.jhonelee"})
+@ComponentScan(basePackages = {"com.yonyou.reconciliation"})
 public class Application {
 	
 	public static void main(String [] args) {
