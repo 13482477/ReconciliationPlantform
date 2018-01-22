5e4624eccdf884065c422ab95f282d25d1753a5e
lizhiqiang <13482477@qq.com>
2018-01-22 10:16:44 +0800

    commit

--- src/main/java/com/yonyou/reconciliation/task/repository/TaskRepository.java
+++ src/main/java/com/yonyou/reconciliation/task/repository/TaskRepository.java
@@ -2,9 +2,11 @@ package com.yonyou.reconciliation.task.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.yonyou.reconciliation.task.entity.Task;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
 }
--- src/main/java/com/yonyou/reconciliation/task/validator/TaskDtoValidator.java
+++ src/main/java/com/yonyou/reconciliation/task/validator/TaskDtoValidator.java
@@ -1,5 +1,22 @@
 package com.yonyou.reconciliation.task.validator;
 
-public class TaskDtoValidator {
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.yonyou.reconciliation.task.dto.TaskDto;
+
+@Component
+public class TaskDtoValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return TaskDto.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		
+	}
 
 }
