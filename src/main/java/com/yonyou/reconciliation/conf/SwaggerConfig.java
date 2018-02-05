71424bff9c40b77df523c0840bfd07ca015a3f49
lizhiqiang <13482477@qq.com>
2018-02-05 10:41:23 +0800

    commit

--- src/main/java/com/yonyou/reconciliation/conf/SwaggerConfig.java
+++ src/main/java/com/yonyou/reconciliation/conf/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 					.apiInfo(apiInfo())
 					.select()
-					.apis(RequestHandlerSelectors.basePackage("com.jhonelee"))
+					.apis(RequestHandlerSelectors.basePackage("com.yonyou"))
 					.paths(PathSelectors.any())
 					.build();
 	}
